27ae1a7472ea6b356c24936a3d167d47634bf1e9
Anshika Garg <147253653+anshikagarga@users.noreply.github.com>
2026-07-22 21:37:09 +0530

    add model

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,31 +1,46 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
     {
         name: {
             type: String,
-            required: true
+            required: [true, "Name is required"],
+            trim: true,
         },
         email: {
             type: String,
-            require: true,
-            unique : true
+            require: [true, "Email is required"],
+            unique : true,
+            lowercase: true,
+            trim: true
         },
         password: {
             type: String,
-            required: true
+            required: [true, "Password is required"],
+            minlength: 6,
         },
         phone: {
-            type: String
+            type: String,
+            required: true,
         },
         role: {
             type: String,
             enum: ["customer", "shopOwner", "admin"],
             default: "customer"
-        }
+        },
+        profileImage: {
+            type: String,
+            default: "",
+        },
+        isVerfied: {
+            type: Boolean,
+            default: false,
+        },
     }, {
         timeStamps: true
     }
 );
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = mongoose.model("user, userSchema");
