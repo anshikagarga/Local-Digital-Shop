47250cdfa6672e132a934bb90648c6594893fd19
Anshika Garg <147253653+anshikagarga@users.noreply.github.com>
2026-07-26 17:40:20 +0530

    add product

--- Backend/middlewares/authMiddleware.js
+++ Backend/middlewares/authMiddleware.js
@@ -1,41 +1,51 @@
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-export const protect = (req, res, next) => {
+export const protect = async (req, res, next) => {
 
-    let token;
+    try {
 
-    if (
-        req.headers.authorization &&
-        req.headers.authorization.startsWith("Bearer")
-    ) {
+        let token;
 
-        token = req.headers.authorization.split(" ")[1];
+        if (
+            req.headers.authorization &&
+            req.headers.authorization.startsWith("Bearer")
+        ) {
 
-        try {
+            token = req.headers.authorization.split(" ")[1];
 
             const decoded = jwt.verify(
                 token,
                 process.env.JWT_SECRET
             );
 
-            req.user = decoded;
+            const user = await User.findById(decoded.id).select("-password");
+
+            if (!user) {
+                return res.status(401).json({
+                    success: false,
+                    message: "User not found"
+                });
+            }
+
+            req.user = user;
 
             next();
 
-        } catch (error) {
+        } else {
 
             return res.status(401).json({
                 success: false,
-                message: "Invalid Token",
+                message: "No token provided"
             });
 
         }
 
-    } else {
+    } catch (error) {
 
         return res.status(401).json({
             success: false,
-            message: "Token not found",
+            message: "Invalid Token"
         });
 
     }
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import {registerUser, loginUser} from "../Controllers/authController.js";
+import {registerUser, loginUser, getProfileUser} from "../Controllers/authController.js";
+import {protect} from "../middlewares/authMiddleware.js"
+
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/profile", protect, getProfileUser);
 
 export default router;
